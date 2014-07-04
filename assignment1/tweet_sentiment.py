879b3c5499da655e1d45cb57d5824d0ad544cbfc
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2014-07-05 00:25:30 +0300

    fixes output.txt (last item was cut off). implements tweet_sentiment.py

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,16 +1,16 @@
 import sys
 import json
+import re
+import operator
 import pprint
 
+scores = {} # initialize an empty dictionary
+pp = pprint.PrettyPrinter(indent=4).pprint
+
 def main():
-    pp = pprint.PrettyPrinter(indent=4).pprint
-
-    sent_file = open(sys.argv[1])
+    afinnfile = open(sys.argv[1])
     tweet_file = sys.argv[2]
-
-    afinnfile = open("AFINN-111.txt")
-    scores = {} # initialize an empty dictionary
-
+    
     for line in afinnfile:
         term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
         scores[unicode(term, 'unicode-escape')] = int(score)  # Convert the score to an integer.
@@ -20,11 +20,24 @@ def main():
         for line in f:
             tweets.append(json.loads(line))
     
+    results = []
     for tweet in tweets:
         if "text" in tweet:
-            pp(tweet["text"]) 
-    
-    pp(scores[u'fuck'])
+            # pp(tweet['text'])
+            score = get_score_for_tweet(tweet['text'])
+            pp(score)
+            results.append((tweet['text'], score,))
+
+    # the most awful tweets last
+    # pp(sorted(results, key=lambda x:x[1], reverse=True))
+
+def get_score_for_tweet(tweet):
+    words = re.findall(r'\w+', tweet.lower())
+    score = reduce(operator.add, [get_score(word) for word in words], 0)
+    return score
+
+def get_score(word):
+    return scores[word] if word in scores else 0
 
 if __name__ == '__main__':
     main()
